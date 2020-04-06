0654875714bd34b0d0436ae05f79a43321989d33
aman <bora.amanalambora@gmail.com>
2020-04-06 15:50:33 +0530

    server-node-mutler

--- node-server/app.js
+++ node-server/app.js
@@ -7,9 +7,26 @@ var storage = multer.diskStorage({
         callback(null, './../resumes');
     },
     filename: function(req, file, callback){
-        callback(null, file.fieldname + "_" + Date.now())
+        callback(null, file.fieldname + "_" + Date.now() + ".pdf")
     }
 });
 
 
+var upload = multer({
+    storage : storage,
+}).single("mypdf")
 
+//mypdf -> attribute of file in view
+
+app.post("/uploadPdf", function(req, res, next){
+
+    upload(req,res,function(err){
+        if(err)res.send(err)
+        else res.send("success")
+    })
+})
+
+app.listen(8080, function(err){
+    if(err)throw err
+    console.log("listening at PORT 8080")
+})
